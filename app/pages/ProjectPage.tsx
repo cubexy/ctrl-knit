da5e10d7bb893a5509dc007ea9ac154d8b3cbb54
Max Niclas Wächtler <54584804+cubexy@users.noreply.github.com>
2025-06-11 22:55:41 +0200

    add VERY VIP implementation of useProject

--- app/pages/ProjectPage.tsx
+++ app/pages/ProjectPage.tsx
@@ -4,53 +4,40 @@ import AddCounterModal from "~/components/ui/modals/AddCounterModal";
 import { useProject } from "~/hooks/useProject";
 
 function ProjectPage() {
-  const project = useProject({
+  const { project, isLoading, error, fetchProject, editCounter, editProject, addCounter, counterStep } = useProject({
     projectId: "12345"
   });
 
-  const data = {
-    name: "Sophie Scarf",
-    url: "https://www.google.com",
-    createdAt: new Date().toISOString(),
-    updatedAt: new Date().toISOString(),
-    counters: [
-      {
-        id: "1",
-        name: "Oberseite",
-        value: 7,
-        stepover: 5
-      },
-      {
-        id: "2",
-        name: "Unterseite",
-        value: 2,
-        stepover: null
-      }
-    ]
-  };
+  if (isLoading) {
+    return <div className="text-center">Loading...</div>;
+  }
+
+  if (error) {
+    return <div className="text-center text-red-500">Error: {error}</div>;
+  }
+
+  if (!project) {
+    return <div className="text-center">Project not found</div>;
+  }
 
   return (
     <>
-      <ProjectHeaderDisplay name={data.name} url={data.url} />
+      <ProjectHeaderDisplay name={project.name} url={project.url} />
       <div className="grid w-full grid-cols-1 gap-4 sm:grid-cols-2">
-        {data.counters.map((counter) => (
+        {project.counters.map((counter) => (
           <CounterDisplay
             key={counter.id}
-            identifier={counter.id}
+            id={counter.id}
             name={counter.name}
-            count={{
-              current: counter.value,
-              target: 12
-            }}
-            stepOver={{
-              current: 3,
-              target: 5
-            }}
-            onIncrement={() => console.log("Increment")}
-            onDecrement={() => console.log("Decrement")}
+            createdAt={counter.createdAt}
+            count={counter.count}
+            stepOver={counter.stepOver}
+            onIncrement={() => counterStep(counter.id, 1)}
+            onDecrement={() => counterStep(counter.id, -1)}
+            onEdit={(update) => editCounter({ ...update, id: counter.id })}
           />
         ))}
-        <AddCounterModal onAddCounter={() => console.log("Add Counter")} />
+        <AddCounterModal onAddCounter={addCounter} />
       </div>
     </>
   );
