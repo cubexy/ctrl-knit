1371fe611d5ce1cc82aecb8749703eb57275b90a
Max Niclas Wächtler <54584804+cubexy@users.noreply.github.com>
2025-06-11 22:58:27 +0200

    add linkicon

--- app/components/ui/displays/ProjectHeaderDisplay.tsx
+++ app/components/ui/displays/ProjectHeaderDisplay.tsx
@@ -1,3 +1,5 @@
+import LinkIcon from "../icons/LinkIcon";
+
 type ProjectHeaderDisplayProps = {
   name: string;
   url?: string;
@@ -15,20 +17,7 @@ function ProjectHeaderDisplay(props: ProjectHeaderDisplayProps) {
             rel="noopener noreferrer"
             className="link flex items-center gap-2 font-mono"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              className="size-4"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244"
-              />
-            </svg>
-
+            <LinkIcon className="size-4" strokeWidth={1} />
             {props.url}
           </a>
         )}
