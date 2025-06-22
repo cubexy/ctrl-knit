5b7fab1c4c5745faaa044e9eaca2f2a531b3614e
Max Niclas Wächtler <54584804+cubexy@users.noreply.github.com>
2025-06-22 22:02:29 +0200

    add theme persistence

--- app/components/layout/RootLayout.tsx
+++ app/components/layout/RootLayout.tsx
@@ -6,7 +6,7 @@ function RootLayout() {
   useDatabase();
 
   return (
-    <div className="flex min-h-screen w-screen justify-center pb-8 transition-colors duration-500">
+    <div className="flex min-h-screen w-screen justify-center pb-8">
       <div className="p5-10 flex w-5xl max-w-5xl flex-col items-center gap-8 px-5 py-4">
         <header className="rounded-box shadow-neutral/15 sticky top-5 z-10 w-full grow-0 py-2 pr-2 pl-4 shadow-sm inset-shadow-xs backdrop-blur-sm">
           <Header />
