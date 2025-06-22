bbfd470c2774530c9e7e2ce542e54d0b7c70e10a
Max Niclas Wächtler <54584804+cubexy@users.noreply.github.com>
2025-06-22 22:33:19 +0200

    remove dangerouslySetInnerHtml

--- app/root.tsx
+++ app/root.tsx
@@ -10,7 +10,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <script dangerouslySetInnerHTML={{ __html: blockingScript }} />
+        <script>{blockingScript}</script>
         <Meta />
         <Links />
         <link rel="icon" href="/favicon.ico" />
