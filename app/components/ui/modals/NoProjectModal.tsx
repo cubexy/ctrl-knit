2ce7ff8d45a1b96fdeea9a706d85a35248f6e5e7
Max Niclas Wächtler <54584804+cubexy@users.noreply.github.com>
2025-06-24 10:17:00 +0200

    place sidebar in seperate file

--- app/components/ui/modals/NoProjectModal.tsx
+++ app/components/ui/modals/NoProjectModal.tsx
@@ -14,7 +14,7 @@ function NoProjectModal() {
       <div className="hero-content text-center">
         <div className="flex max-w-lg flex-col items-center">
           <WoolIcon className="fill-base-300 size-36" strokeWidth={1} />
-          <h1 className="text-4xl font-bold">Kein offenes Projekt </h1>
+          <h1 className="text-4xl font-bold">Kein offenes Projekt</h1>
           <p className="text-md pt-2 pb-3">
             Du hast gerade noch kein Projekt geöffnet. Öffne oben über "Projekte" ein schon erstelltes Projekt oder fang
             hier ein neues an!
