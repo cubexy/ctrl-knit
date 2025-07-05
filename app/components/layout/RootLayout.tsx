b1a11d772ea3f62e9b55d27d52de926bcf03d8b1
Max Niclas Wächtler <54584804+cubexy@users.noreply.github.com>
2025-07-05 11:03:16 +0200

    add scrollbar gutter

--- app/components/layout/RootLayout.tsx
+++ app/components/layout/RootLayout.tsx
@@ -8,7 +8,10 @@ function RootLayout() {
       <div className="min-h-full max-w-0 grow transition-normal duration-300 ease-in-out lg:max-w-80 lg:p-5 lg:pr-0">
         <Sidebar />
       </div>
-      <div className="p5-10 flex max-h-screen flex-grow flex-col items-center gap-8 overflow-y-auto px-5 py-4">
+      <div
+        className="p5-10 flex max-h-screen flex-grow flex-col items-center gap-8 overflow-y-auto py-4 pr-2 pl-5"
+        style={{ scrollbarGutter: "stable right" }}
+      >
         <header className="rounded-box shadow-neutral/15 sticky top-1 z-10 w-full grow-0 py-2 pr-2 pl-4 shadow-sm inset-shadow-xs backdrop-blur-sm">
           <Header />
         </header>
