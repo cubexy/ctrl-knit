a41008b6d50bbb5b47601d8db90c64b444a2d9fe
Max Niclas Wächtler <54584804+cubexy@users.noreply.github.com>
2025-07-05 11:04:12 +0200

    fix string

--- app/components/layout/RootLayout.tsx
+++ app/components/layout/RootLayout.tsx
@@ -9,8 +9,8 @@ function RootLayout() {
         <Sidebar />
       </div>
       <div
-        className="p5-10 flex max-h-screen flex-grow flex-col items-center gap-8 overflow-y-auto py-4 pr-2 pl-5"
-        style={{ scrollbarGutter: "stable right" }}
+        className="p5-10 flex max-h-screen flex-grow flex-col items-center gap-8 overflow-y-auto py-4 pr-1 pl-5"
+        style={{ scrollbarGutter: "stable" }}
       >
         <header className="rounded-box shadow-neutral/15 sticky top-1 z-10 w-full grow-0 py-2 pr-2 pl-4 shadow-sm inset-shadow-xs backdrop-blur-sm">
           <Header />
