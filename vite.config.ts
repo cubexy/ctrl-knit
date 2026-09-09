8663f74740c6d70c2398c329dab2502560cc063e
Max Niclas Wächtler <54584804+cubexy@users.noreply.github.com>
2026-09-09 22:48:27 +0200

    update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import commonjs from "vite-plugin-commonjs";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), commonjs()],
+  plugins: [tailwindcss(), reactRouter(), commonjs()],
   define: {
     APP_VERSION: JSON.stringify(process.env.npm_package_version)
+  },
+  resolve: {
+    tsconfigPaths: true
   }
 });
